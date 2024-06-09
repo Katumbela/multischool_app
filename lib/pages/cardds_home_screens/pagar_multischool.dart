d540167241490d48bb870fac40c4e7d9bbf3a12f
Katumbela <ja3328173@gmail.com>
2024-06-09 10:16:57 +0100

    3 paginas

--- lib/pages/cardds_home_screens/pagar_multischool.dart
+++ lib/pages/cardds_home_screens/pagar_multischool.dart
@@ -14,6 +14,7 @@ class _PagarMultischoolState extends State<PagarMultischool> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           CardScreenHeader(),
@@ -23,19 +24,47 @@ class _PagarMultischoolState extends State<PagarMultischool> {
               children: [
                 Container(
                   margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * .18,
+                    top: MediaQuery.of(context).size.height * .15,
                     left: 20,
                     right: 20,
                   ),
                   width: MediaQuery.of(context).size.width,
                   height: 150,
                   decoration: BoxDecoration(
-                      color: Colors.grey[300],
+                      color: Colors.grey[200],
+                      //image: AssetImage(assetName),
+                      boxShadow: [
+                        BoxShadow(
+                            color: AppColors.primary.withOpacity(.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3))
+                      ],
                       borderRadius: BorderRadius.circular(15)),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [Text('CONTA')],
+                    children: [
+                      Text(
+                        'CONTA',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        '100 324 3434 1 000',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        '190 450.00 kz',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
+                    ],
                   ),
                 ),
               ],
